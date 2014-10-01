ad6d481749369962fefe82e32fc0a0c99e557c81
kc5nra <jrb@turrettech.com>
2014-09-30 23:01:54 -0500

    Switch from html to rtf for readme

--- install/osx/gen_changelog.py
+++ install/osx/gen_changelog.py
@@ -7,12 +7,11 @@ def gen_html(github_user):
     latest_tag = cmd('git describe --tags --abbrev=0')
     rev = cmd('git rev-parse {0}'.format(latest_tag))
     anno = cmd('git cat-file -p {0}'.format(rev))
-    url = 'https://github.com/{0}/obs-project/commit/%H'.format(github_user)
+    url = 'https://github.com/{0}/obs-studio/commit/%H'.format(github_user)
 
     with open('readme.html', 'w') as f:
         f.write("<html><body>")
-
-        log_cmd = 'git log {0}...HEAD --pretty=format:\'<li>&bull; <a href="{1}">(view)</a> %s</li>\''
+        log_cmd = """git log {0}...HEAD --pretty=format:'<li>&bull; <a href="{1}">(view)</a> %s</li>'"""
         log_res = cmd(log_cmd.format(latest_tag, url))
         if len(log_res.splitlines()):
             f.write('<p>Changes since {0}: (Newest to oldest)</p>'.format(latest_tag))
@@ -41,6 +40,9 @@ def gen_html(github_user):
         if ul:
             f.write('</ul>')
         f.write('</p></body></html>')
+
+    cmd('textutil -convert rtf readme.html -output readme.rtf')
+    cmd("""sed -i '' 's/Times-Roman/Verdana/g' readme.rtf""")
 
 import argparse
 parser = argparse.ArgumentParser(description='obs-studio readme gen')
