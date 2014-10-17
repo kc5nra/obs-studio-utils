62c758fd95cea418a534c3356a6e00f73678be94
John R. Bradley <jrbradley@gmail.com>
2014-10-17 16:51:48 -0500

    Merge to one file

--- release/osx/feed_util.py
+++ release/osx/feed_util.py
@@ -3,6 +3,29 @@ import os
 def get_sub(dir):
     return [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
 
+def cmd(cmd):
+    import subprocess
+    import shlex
+    return subprocess.check_output(shlex.split(cmd)).rstrip('\r\n')
+
+def qn_tag(n, t):
+    return {
+        'ce': str(ET.QName('http://catchexception.org/xml-namespaces/ce', t)),
+        'sparkle': str(ET.QName('http://www.andymatuschak.org/xml-namespaces/sparkle', t))
+    }[n]
+
+def get_last_item(f):
+    t = ET.ElementTree()
+    t.parse(f)
+    # assume there is a valid version if updates.xml exists
+    return t.findall('channel/item')[-1]
+
+def get_remote_channels(user, url):
+    heads_raw = cmd('git ls-remote --heads {0}'.format(url))
+
+    import re
+    return ['{0}/{1}'.format(user, x) for x in re.findall(r'[a-f, 0-9]{40}\s+?refs/heads/(.*)', heads_raw)]
+
 import argparse
 parser = argparse.ArgumentParser(description='obs-studio release util')
 parser.add_argument('-d', '--directory', dest='dir', default='.')
@@ -17,24 +40,12 @@ from xml.etree import ElementTree as ET
 ET.register_namespace('sparkle', 'http://www.andymatuschak.org/xml-namespaces/sparkle')
 ET.register_namespace('ce', 'http://catchexception.org/xml-namespaces/ce')
 
-def qn_tag(n, t):
-    return {
-        'ce': str(ET.QName('http://catchexception.org/xml-namespaces/ce', t)),
-        'sparkle': str(ET.QName('http://www.andymatuschak.org/xml-namespaces/sparkle', t))
-    }[n]
-
-def get_last_item(f):
-    t = ET.ElementTree()
-    t.parse(f)
-    # assume there is a valid version if updates.xml exists
-    return t.findall('channel/item')[-1]
 
 git_url = 'https://github.com/{0}/obs-studio.git'
 
 stale_channels = []
 for d in get_sub('.'):
     if d != 'stable':
-        from get_remote_channels import get_remote_channels
         valid_channels = get_remote_channels(d, git_url.format(d))
     for root, dirs, files in os.walk(d):
       for f in fnmatch.filter(files, 'updates.xml'):
